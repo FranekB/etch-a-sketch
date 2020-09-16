33d49c1dcb321d8280d7b7e76a702234ef35af62
FranekB <camil.bodziony@gmail.com>
2020-09-17 00:11:36 +0200

    Added ability to choose grid size

--- index.js
+++ index.js
@@ -2,27 +2,26 @@
 const container = document.querySelector('#game-container');
 const side = container.clientWidth;
 
-
-for(let i = 0; i < 256; i++){
-  let div = document.createElement('div');
-
-  //add styling
-  div.style.width = side / 16 + "px";
-  div.style.height = side / 16 + "px";
-  div.style.boxSizing = "border-box";
-  div.style.border = "1px dotted grey";
-  container.appendChild(div);
-}
-
-
-//Add listeners for hovering over cells of a Grid
-let cells = document.querySelectorAll("#game-container > div")
-cells.forEach(cell => cell.addEventListener("mouseover", changeColor))
-
-
 //Give ability to change grid resolution
 let newGrid = document.getElementById("new-grid")
 newGrid.addEventListener("click", setNewGrid)
+
+
+function createNewGrid(cellsWidth){
+  for(let i = 0; i < cellsWidth * cellsWidth; i++){
+    let div = document.createElement('div');
+
+    //add styling
+    div.style.width = side / cellsWidth + "px";
+    div.style.height = side / cellsWidth + "px";
+    div.style.boxSizing = "border-box";
+    div.style.border = "1px dotted grey";
+    container.appendChild(div);
+    container.style.gridTemplate = `repeat(${cellsWidth}, 1fr) / repeat(${cellsWidth}, 1fr)`;
+  }
+  let cells = document.querySelectorAll("#game-container > div")
+  cells.forEach(cell => cell.addEventListener("mouseover", changeColor))
+}
 
 
 function changeColor(e){
@@ -30,15 +29,25 @@ function changeColor(e){
 }
 
 function setNewGrid(){
-  let resolution = +prompt("What resolution of a grid you want to have?");
+  let resolution = +prompt("What resolution of a grid you want to have?(Choose value between 1 and 100)");
+  let cells = document.querySelectorAll("#game-container > div")
   //If user enter not a number give alert and don't change anything
   if(isNaN(resolution)){
     alert("You must enter a number!");
     return
   }
-  //remove old grid
-  cells.forEach(cell => {
-    cell.style.width = side / resolution + "px";
-    cell.style.height = side / resolution + "px";
-  })
+  else if(resolution > 100){
+    alert("Number is too big! Choose number between 1 - 100.")
+    return
+  }
+  //change width and height of cells + grid-template-rows
+  cells.forEach(cells => cells.remove())
+  createNewGrid(resolution)
+
 }
+
+
+
+
+//create first gridTemplate
+createNewGrid(16)
