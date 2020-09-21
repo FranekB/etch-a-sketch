a0af04e408df0c526054f00b8f4c1ea07546cf8a
FranekB <camil.bodziony@gmail.com>
2020-09-21 19:39:25 +0200

    Added reset button and rainbow mode

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-//Make 16 x 16 div
-const container = document.querySelector('#game-container');
-const side = container.clientWidth;
+
+
+
+//BUTTONS//
+
 
 //Give ability to change grid resolution
-let newGrid = document.getElementById("new-grid")
-newGrid.addEventListener("click", setNewGrid)
+let newGridButton = document.getElementById("new-grid");
+newGridButton.addEventListener("click", changeGridSize);
 
+//Give ability to reset the grid(clearing all spots)
+let resetGridButton = document.getElementById("reset");
+resetGridButton.addEventListener("click", resetGrid)
 
-function createNewGrid(cellsWidth){
-  for(let i = 0; i < cellsWidth * cellsWidth; i++){
-    let div = document.createElement('div');
-
-    //add styling
-    div.style.width = side / cellsWidth + "px";
-    div.style.height = side / cellsWidth + "px";
-    div.style.boxSizing = "border-box";
-    div.style.border = "1px dotted grey";
-    container.appendChild(div);
-    container.style.gridTemplate = `repeat(${cellsWidth}, 1fr) / repeat(${cellsWidth}, 1fr)`;
-  }
-  let cells = document.querySelectorAll("#game-container > div")
-  cells.forEach(cell => cell.addEventListener("mouseover", changeColor))
+//Reset grid
+function resetGrid(){
+  let cells = document.querySelectorAll("#game-container > div");
+  cells.forEach(cell => cell.style.backgroundColor = "white");
 }
 
+//Setting mode to Black
+let blackModeButton = document.getElementById("black");
+blackModeButton.addEventListener("click", () =>{
+  mode = "black";
+})
 
-function changeColor(e){
-  e.target.style.backgroundColor = "black";
-}
+//setting mode to Rainbow
+let rainbowModeButton = document.getElementById("rainbow");
+rainbowModeButton.addEventListener("click", () =>{
+  mode = "rainbow";
+})
 
-function setNewGrid(){
+
+//removes old grid and calls a function for a new grid
+function changeGridSize(){
   let resolution = +prompt("What resolution of a grid you want to have?(Choose value between 1 and 100)");
   let cells = document.querySelectorAll("#game-container > div")
   //If user enter not a number give alert and don't change anything
@@ -43,11 +47,49 @@ function setNewGrid(){
   //change width and height of cells + grid-template-rows
   cells.forEach(cells => cells.remove())
   createNewGrid(resolution)
+}
 
+
+//creates a new grid
+function createNewGrid(cellsWidth){
+  const container = document.querySelector('#game-container');
+  const side = container.clientWidth;
+  for(let i = 0; i < cellsWidth * cellsWidth; i++){
+    let div = document.createElement('div');
+
+    //add styling
+    div.style.width = side / cellsWidth + "px";
+    div.style.height = side / cellsWidth + "px";
+    div.style.boxSizing = "border-box";
+    div.style.border = "1px dotted grey";
+    container.appendChild(div);
+    container.style.gridTemplate = `repeat(${cellsWidth}, 1fr) / repeat(${cellsWidth}, 1fr)`;
+  }
+  let cells = document.querySelectorAll("#game-container > div");
+  cells.forEach(cell => cell.addEventListener("mouseover", changeColor));
 }
 
 
 
 
-//create first gridTemplate
+
+
+//CHANGING COLORS
+
+//changes color of a cell after hover
+function changeColor(e){
+  if(mode == "black"){
+    e.target.style.backgroundColor = "black";
+  }
+
+  else if(mode == "rainbow"){
+    e.target.style.backgroundColor = "red";
+  }
+}
+
+
+
+
+//STARTING GRID
 createNewGrid(16)
+let mode = "black"
